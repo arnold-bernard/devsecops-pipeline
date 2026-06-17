a8ceacea3f8c53d14d276e7f75759ceb3d4daf02
Arnold Bernard <arnoldbernard1101@gmail.com>
2026-06-17 13:16:16 +0530

    Added a new IAM tf file , created a new prfile for the ec2 demo instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "demo" {
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   security_groups = [aws_security_group.web.name]
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
